1f71b1f4553f0099f87b0a9c1a3056cdc517ea64
na-ha <33252395+na-ha@users.noreply.github.com>
2022-01-22 16:48:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 document.getElementById("more").addEventListener("click", function () {
-  document.getElementById("details").style.display = "block";
+  document.getElementById("details").style.display = "inline";
   document.getElementById("more").style.display = "none";
-  document.getElementById("less").style.display = "block";
+  document.getElementById("less").style.display = "inline";
 });
 
 document.getElementById("less").addEventListener("click", function () {
   document.getElementById("details").style.display = "none";
-  document.getElementById("more").style.display = "block";
+  document.getElementById("more").style.display = "inline";
   document.getElementById("less").style.display = "none";
 });
